554186662971b7af65ab64a8afe6fad1b22cca5d
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-02-24 16:26:45 +0300

    first commit

--- TasarimCalismasi/ViewController.swift
+++ TasarimCalismasi/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Pizza"
+        let appearance = UINavigationBarAppearance()
+        //Arka Plan rengi
+        appearance.backgroundColor = UIColor.init(named: "anaRenk")
+        //Başlık Rengi
+        appearance.titleTextAttributes = [.foregroundColor:UIColor(named: "yaziRenk1")!,
+        NSAttributedString.Key.font:UIFont(name: "Pacifico-Regular", size: 22)!]
+        navigationController?.navigationBar.isTranslucent = true
+        //Status Bar rengi değişimi
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
 
 
